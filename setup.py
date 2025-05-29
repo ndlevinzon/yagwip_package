9368e8be2d51240640d7c3153912ac65dcf979f5
ndlevinzon <59805413+ndlevinzon@users.noreply.github.com>
2025-05-29 10:37:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     python_requires='>=3.6',
     entry_points={
         "console_scripts": [
-            "yagwip = yagwip:main",  # command → module:function
+            "yagwip = yagwip:main",
         ],
     },
 )
