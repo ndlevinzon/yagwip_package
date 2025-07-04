a7ba44a05a37c0e4312c2f60ddf6e485944393a8
ndlevinzon <ndlevinzon@gmail.com>
2025-07-04 12:40:36 -0400

    Version 2.0

--- src/yagwip/yagwip.py
+++ src/yagwip/yagwip.py
@@ -298,7 +298,7 @@ class YagwipShell(cmd.Cmd, LoggingMixin):
                         multiplicity=multiplicity,
                     )
                     # Run ORCA Geometry Optimization
-                    ligand_pipeline.run_orca(orca_path, "orca/ligand.inp")
+                    ligand_pipeline.run_orca(orca_path, "./orca/ligand.inp")
                     # Append atom charges to mol2
                     ligand_pipeline.apply_orca_charges_to_mol2(mol2_file, "orca/ligand.property.txt")
                     ligand_pipeline.run_parmchk2(mol2_file)  # creates ligand.frcmod
